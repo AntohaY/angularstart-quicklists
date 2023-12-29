5272b510569562b39f27f85aba9040321515c63b
antonyakovenko <anton.yakovenko@one-inside.com>
2023-12-29 15:37:35 +0200

    feat: added additional features to create checklist items in checklists and checklist items detail pages

---
+++ src/app/checklist/ui/checklist-header.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Checklist } from '../../shared/interfaces/checklist';
+
+@Component({
+  standalone: true,
+  selector: 'app-checklist-header',
+  template: `
+    <header>
+      <a routerLink="/home">Back</a>
+      <h1>
+        {{ checklist.title }}
+      </h1>
+      <div>
+        <button (click)="addItem.emit()">Add item</button>
+      </div>
+    </header>
+  `,
+  imports: [RouterLink],
+})
+export class ChecklistHeaderComponent {
+  @Input({ required: true }) checklist!: Checklist;
+  @Output() addItem = new EventEmitter<void>();
+
+}
